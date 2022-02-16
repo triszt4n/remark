4ddd8ef5d280fa9993cce7010bb3ad584f51b8ee
triszt4n <trisztanpiller@gmail.com>
2022-02-16 19:55:15 +0100

    Small changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { Box, ChakraProvider, Code, Grid, Link, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, ChakraProvider, Code, Grid, Text, VStack } from '@chakra-ui/react'
 import '@fontsource/inter/300.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
 import '@fontsource/inter/900.css'
 import * as React from 'react'
+import customTheme from './assets/theme'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { Logo } from './Logo'
-import customTheme from './util/theme'
 
 export const App = () => (
   <ChakraProvider theme={customTheme}>
@@ -14,13 +14,11 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
+          <Logo height="20rem" />
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
-            Learn Chakra
-          </Link>
+          <Button colorScheme="theme">Learn Chakra</Button>
         </VStack>
       </Grid>
     </Box>
