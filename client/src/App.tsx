b793597201b20c5bdca34375eb72f177300a7983
triszt4n <trisztanpiller@gmail.com>
2022-02-16 19:36:31 +0100

    More client code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import { Box, ChakraProvider, Code, Grid, Link, Text, theme, VStack } from '@chakra-ui/react'
+import { Box, ChakraProvider, Code, Grid, Link, Text, VStack } from '@chakra-ui/react'
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/900.css'
 import * as React from 'react'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { Logo } from './Logo'
+import customTheme from './util/theme'
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
+  <ChakraProvider theme={customTheme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
