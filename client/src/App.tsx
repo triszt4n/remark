c9a4831659dd2bcf5557c61b8e4d4a6be963041e
triszt4n <trisztanpiller@gmail.com>
2022-03-14 01:53:01 +0100

    API on the way

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,37 @@ import '@fontsource/inter/300.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
 import '@fontsource/inter/900.css'
-import * as React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import customTheme from './assets/theme'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { Logo } from './Logo'
+import { API_HOST } from './util/environment'
+import { generateName } from './util/name-generator'
 
-export const App = () => (
-  <ChakraProvider theme={customTheme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo height="20rem" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Button colorScheme="theme">Learn Chakra</Button>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  const [message, setMessage] = useState<string>('')
+  useEffect(() => {
+    axios.get<string>(`${API_HOST}/posts/GetPosts?name=${generateName()}`).then((response) => {
+      setMessage(response.data)
+    })
+  }, [])
+
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo height="16rem" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            </Text>
+            <Text>{message}</Text>
+            <Button colorScheme="theme">Learn Chakra</Button>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
--- client/src/Logo.tsx
+++ client/src/Logo.tsx
@@ -1,5 +1,5 @@
 import { useColorModeValue } from '@chakra-ui/react'
-import * as React from 'react'
+import React from 'react'
 import { ReactComponent as LogoComponent } from './assets/svg/remark-logo-big.svg'
 
 export const Logo: React.FC<any> = (props) => <LogoComponent {...props} fill={useColorModeValue('black', 'white')} />
