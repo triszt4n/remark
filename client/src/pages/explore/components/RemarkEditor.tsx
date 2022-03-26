7609c5d614f954635a8c177f2a08fe296a3134b9
triszt4n <trisztanpiller@gmail.com>
2022-03-26 16:27:17 +0100

    Experiment with react-markdown

--- client/src/pages/explore/components/RemarkEditor.tsx
+++ client/src/pages/explore/components/RemarkEditor.tsx
@@ -1,12 +1,37 @@
-import { FC, useState } from 'react'
-import SimpleMdeReact from 'react-simplemde-editor'
+import { FC } from 'react'
+import ReactMarkdown, { Components } from 'react-markdown'
 
 export const RemarkEditor: FC = () => {
-  const [value, setValue] = useState('Initial value')
-
-  const onChange = (value: string) => {
-    setValue(value)
+  const componentOptions: Components = {
+    // Map `h1` (`# heading`) to use `h2`s.
+    h1: 'h1'
   }
 
-  return <SimpleMdeReact value={value} onChange={onChange} />
+  const markdown = `
+## Overview
+
+* **Follows** [CommonMark](https://commonmark.org)
+* Renders *actual* React elements instead of using \`dangerouslySetInnerHTML\`
+* Lets you define your own components (to render \`MyHeading\` instead of \`h1\`)
+* Has a lot of plugins
+
+## Syntax highlighting
+
+Here is an example of a plugin to highlight code:
+[\`rehype-highlight\`](https://github.com/rehypejs/rehype-highlight).
+
+\`\`\`js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactMarkdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+
+ReactDOM.render(
+  <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{'# Your markdown here'}</ReactMarkdown>,
+  document.querySelector('#content')
+)
+\`\`\`
+  `
+
+  return <ReactMarkdown components={componentOptions} children={markdown} />
 }
