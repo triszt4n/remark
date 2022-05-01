dfed8119b6096e969478a071631fcd519c417034
triszt4n <trisztanpiller@gmail.com>
2022-05-02 00:04:55 +0200

    Add RemoveModerator

---
+++ functions/remark-functions-channels/RemoveModeratorFromChannel/index.ts
@@ -0,0 +1,68 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { readUserFromAuthHeader } from '@triszt4n/remark-auth'
+import { fetchCosmosContainer, fetchCosmosDatabase } from '../lib/dbConfig'
+import { ChannelResource } from '../lib/model'
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+  const id = context.bindingData.id as string
+
+  // Authorization
+  const result = readUserFromAuthHeader(req, process.env.JWT_PRIVATE_KEY)
+  if (result.isError) {
+    context.res = {
+      status: result.status,
+      body: { message: result.message }
+    }
+    return
+  }
+  const { userFromJwt: user } = result
+
+  const { moderatorId } = req.body as { moderatorId: string }
+  if (!moderatorId) {
+    context.res = {
+      status: 400,
+      body: { message: `Request body field(s) failed validation.` }
+    }
+    return
+  }
+
+  const database = fetchCosmosDatabase()
+  const channelsContainer = fetchCosmosContainer(database, 'Channels')
+  const channelItem = channelsContainer.item(id, id)
+  let { resource: channel } = await channelItem.read<ChannelResource>()
+  if (!channel) {
+    context.res = {
+      status: 404,
+      body: { message: `Channel with id ${id} not found.` }
+    }
+    return
+  }
+
+  // Does channel have this moderator
+  if (!channel.moderatorIds.includes(moderatorId)) {
+    context.res = {
+      status: 400,
+      body: { message: 'Moderator id is not an id of a moderator!' }
+    }
+    return
+  }
+
+  // Check permissions
+  if (channel.ownerId != user.id) {
+    context.res = {
+      status: 403,
+      body: { message: 'You are forbidden to make changes this channel!' }
+    }
+    return
+  }
+
+  // Apply changes
+  channel.moderatorIds.splice(channel.moderatorIds.indexOf(moderatorId), 1)
+  const { resource: updatedChannel } = await channelItem.replace<ChannelResource>(channel)
+
+  context.res = {
+    body: updatedChannel
+  }
+}
+
+export default httpTrigger
