bea303f8b0af1dac2d26794a15bc03e607f47740
triszt4n <trisztanpiller@gmail.com>
2022-11-03 20:16:43 +0100

    Start TF

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.29.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "remark-dev-rg" {
+  name     = "remark-dev-rg"
+  location = "eu-west"
+}
