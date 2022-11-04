f3de7ab5fda8123b6541a3447db871a3d16f9025
triszt4n <trisztanpiller@gmail.com>
2022-11-04 01:27:42 +0100

    Rework Users API for new Google auth JS

--- functions/remark-functions-users/LoginUser/index.ts
+++ functions/remark-functions-users/LoginUser/index.ts
@@ -1,12 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import { createJWT } from '@triszt4n/remark-auth'
-import axios from 'axios'
+import * as jwt from 'jsonwebtoken'
 import { fetchCosmosContainer } from '../lib/dbConfig'
 import { getOrCreateUserByEmail } from '../lib/dbQueries'
+import { GoogleUser } from '../lib/model'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   // Request body verification
-  if (!req.body || !req.body.accessToken) {
+  if (!req.body || !req.body.credential) {
     context.res = {
       status: 400,
       body: { message: `Bad request: No accessToken in request body!` }
@@ -14,33 +15,18 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     return
   }
 
-  const { accessToken } = req.body
+  const { credential } = req.body
+  const googleUser = jwt.decode(credential) as GoogleUser
   const usersContainer = fetchCosmosContainer('Users')
+  const dbUser = await getOrCreateUserByEmail(usersContainer, googleUser)
+  const jwtToken = createJWT(dbUser, process.env.JWT_PRIVATE_KEY, 60 * 60 * 24 * 2) // two days
 
-  // Query from Google Resource server
-  await axios
-    .get('https://www.googleapis.com/oauth2/v3/userinfo', {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    })
-    .then(async (response) => {
-      const googleUser = response.data
-
-      const dbUser = await getOrCreateUserByEmail(usersContainer, googleUser)
-      const jwtToken = createJWT(dbUser, process.env.JWT_PRIVATE_KEY, 60 * 60 * 24 * 2) // two days
-
-      context.res = {
-        body: {
-          jwt: jwtToken,
-          user: dbUser
-        }
-      }
-    })
-    .catch((error) => {
-      context.log('[ERROR] at LoginUser', error)
-      context.res = {
-        body: { message: `Bad request for google oauth endpoint!` }
-      }
-    })
+  context.res = {
+    body: {
+      jwt: jwtToken,
+      user: dbUser
+    }
+  }
 }
 
 export default httpTrigger
