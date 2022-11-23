8e807031173ea7c0ddf7893b079580e202dcdc83
triszt4n <trisztanpiller@gmail.com>
2022-11-23 01:08:55 +0100

    Using backend id

--- infrastructure/modules/function-app-with-api/api.tf
+++ infrastructure/modules/function-app-with-api/api.tf
@@ -9,44 +9,33 @@ resource "azurerm_api_management_api" "api" {
   subscription_required = false
 }
 
+data "azurerm_function_app_host_keys" "keys" {
+  name                = azurerm_windows_function_app.function-app.name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_api_management_backend" "backend" {
+  name                = "remark-dev-${var.name}-backend"
+  resource_group_name = var.resource_group_name
+  api_management_name = var.apim_name
+  protocol            = "http"
+  url                 = "https://${azurerm_windows_function_app.function-app.default_hostname}/api/"
+  credentials {
+    header = {
+      "x-functions-key" = "${data.azurerm_function_app_host_keys.keys.default_function_key}"
+    }
+  }
+}
+
 resource "azurerm_api_management_api_policy" "api-policy" {
   api_name            = azurerm_api_management_api.api.name
   api_management_name = var.apim_name
   resource_group_name = var.resource_group_name
 
-  xml_content = <<XML
-<policies>
-    <inbound>
-        <base />
-        <cors allow-credentials="false">
-            <allowed-origins>
-                <origin>${var.local_dev_server_url}</origin>
-                <origin>https://${var.custom_domain}</origin>
-                <origin>https://${var.web_app_hostname}</origin>
-            </allowed-origins>
-            <allowed-methods>
-                <method>GET</method>
-                <method>POST</method>
-                <method>DELETE</method>
-                <method>PATCH</method>
-            </allowed-methods>
-            <allowed-headers>
-                <header>*</header>
-            </allowed-headers>
-            <expose-headers>
-                <header>*</header>
-            </expose-headers>
-        </cors>
-    </inbound>
-    <backend>
-        <base />
-    </backend>
-    <outbound>
-        <base />
-    </outbound>
-    <on-error>
-        <base />
-    </on-error>
-</policies>
-XML
+  xml_content = templatefile("${path.module}/xmls/api-policy.xml", {
+    local_dev_server_url = var.local_dev_server_url
+    custom_domain        = var.custom_domain
+    web_app_hostname     = var.web_app_hostname
+    backend_id           = azurerm_api_management_backend.backend.name
+  })
 }
