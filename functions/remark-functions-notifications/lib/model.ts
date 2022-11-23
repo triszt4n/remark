19dcae01d210c66fd4143bc691396c2335d2bbf5
triszt4n <trisztanpiller@gmail.com>
2022-11-23 18:09:07 +0100

    Update notif backend

--- functions/remark-functions-notifications/lib/model.ts
+++ functions/remark-functions-notifications/lib/model.ts
@@ -1,9 +1,4 @@
 import { Resource } from '@azure/cosmos'
-import { CreateNotificationView, NotificationModel } from '@triszt4n/remark-types'
-import validator from 'validator'
+import { NotificationModel } from '@triszt4n/remark-types'
 
 export type NotificationResource = NotificationModel & Resource
-
-export const validateInput = ({ messageTitle, messageBody, userId }: CreateNotificationView): boolean => {
-  return [validator.isLength(messageTitle, { min: 1, max: 120 }), validator.isLength(messageBody, { min: 1, max: 1000 })].every(Boolean)
-}
